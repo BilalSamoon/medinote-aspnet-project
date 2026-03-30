5ca879ca7074dc1830e228a2253d902d98db8aa0
BilalSamoon <gilljohnson213@gmail.com>
2026-03-30 03:53:00 -0400

    Add NUnit tests for admin services

---
+++ MediNote/MediNote.Tests/AdminReportServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using MediNote.Web.Services;
+using NUnit.Framework;
+
+namespace MediNote.Tests
+{
+    /// Author: Bilal Ahmed Samoon
+    /// NUnit tests for AdminReportService.
+    public class AdminReportServiceTests
+    {
+        private AdminReportService _service = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            _service = new AdminReportService();
+        }
+
+        [Test]
+        public void GetTotalAppointments_ReturnsCorrectValue()
+        {
+            var result = _service.GetTotalAppointments(10);
+
+            Assert.That(result, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void GetPendingAppointments_ReturnsCorrectValue()
+        {
+            var result = _service.GetPendingAppointments(5);
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+    }
+}
